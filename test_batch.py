bfc0acab5ec56a87619026f122646c401fcd01c7
Jack Crowson <crowstar@localhost.localdomain>
2018-06-30 19:58:24 +0100

    make UNIT save test images in the same way as cycleGAN

--- test_batch.py
+++ test_batch.py
@@ -16,19 +16,20 @@ except ImportError: # will be 3.x series
 import sys
 import torch
 import os
+# imports for cycleGAN codebase
+from util import html
+from util.visualizer import save_images
+from collections import OrderedDict
 
-
+# input_folder should contain testA and testB folders
+# output_folder does not need to include results/folder, just folder
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='configs/edges2handbags_folder', help='Path to the config file.')
 parser.add_argument('--input_folder', type=str, help="input image folder")
 parser.add_argument('--output_folder', type=str, help="output image folder")
 parser.add_argument('--checkpoint', type=str, help="checkpoint of autoencoders")
 parser.add_argument('--seed', type=int, default=1, help="random seed")
-parser.add_argument('--num_style',type=int, default=10, help="number of styles to sample")
-parser.add_argument('--synchronized', action='store_true', help="whether use synchronized style code or not")
-parser.add_argument('--output_only', action='store_true', help="whether use synchronized style code or not")
 parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
-parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
 
 opts = parser.parse_args()
 
@@ -39,19 +40,21 @@ torch.cuda.manual_seed(opts.seed)
 config = get_config(opts.config)
 input_dim = config['input_dim_a']
 
-# Setup model and data loader
-image_names = ImageFolder(opts.input_folder, transform=None, return_paths=True)
-data_loader = get_data_loader_folder(opts.input_folder, 1, False, new_size=config['new_size'], crop=False)
+# Setup data directories
+folder_A = os.path.join(opts.input_folder, 'testA')
+folder_B = os.path.join(opts.input_folder, 'testB')
 
+# Setup data loaders
+names_A = ImageFolder(folder_A, transform=None, return_paths=True)
+names_B = ImageFolder(folder_B, transform=None, return_paths=True)
+
+data_loader_A = get_data_loader_folder(folder_A, 1, False, new_size=config['new_size'], crop=False)
+data_loader_B = get_data_loader_folder(folder_B, 1, False, new_size=config['new_size'], crop=False)
+
+# Setup model
 config['vgg_model_path'] = opts.output_path
-if opts.trainer == 'MUNIT':
-    style_dim = config['gen']['style_dim']
-    trainer = MUNIT_Trainer(config)
-elif opts.trainer == 'UNIT':
-    trainer = UNIT_Trainer(config)
-else:
-    sys.exit("Only support MUNIT|UNIT")
 
+trainer = UNIT_Trainer(config)
 
 state_dict = torch.load(opts.checkpoint)
 trainer.gen_a.load_state_dict(state_dict['a'])
@@ -67,62 +70,59 @@ decode_z2b = trainer.gen_b.decode # decode to B
 encode_b2z = trainer.gen_b.encode # encode from B
 decode_z2a = trainer.gen_a.decode # decode to A
 
-if opts.trainer == 'MUNIT':
-    # Start testing
-    style_fixed = Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda(), volatile=True)
-    for i, (images, names) in enumerate(zip(data_loader,image_names)):
-        print(names[1])
-        images = Variable(images.cuda(), volatile=True)
-        content, _ = encode(images)
-        style = style_fixed if opts.synchronized else Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda(), volatile=True)
-        for j in range(opts.num_style):
-            s = style[j].unsqueeze(0)
-            outputs = decode(content, s)
-            outputs = (outputs + 1) / 2.
-            # path = os.path.join(opts.output_folder, 'input{:03d}_output{:03d}.jpg'.format(i, j))
-            basename = os.path.basename(names[1])
-            path = os.path.join(opts.output_folder+"_%02d"%j,basename)
-            if not os.path.exists(os.path.dirname(path)):
-                os.makedirs(os.path.dirname(path))
-            vutils.save_image(outputs.data, path, padding=0, normalize=True)
-        if not opts.output_only:
-            # also save input images
-            vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0, normalize=True)
-elif opts.trainer == 'UNIT':
-    # Start testing
-    for i, (images, names) in enumerate(zip(data_loader,image_names)):
-        print(names[1])
+# create website
+web_dir = os.path.join('results', opts.output_folder, 'test_latest')
+webpage = html.HTML(web_dir, 'Experiment = %s, Phase = Test' % opts.output_folder)
 
-        # encode a to latent space
-        images = Variable(images.cuda(), volatile=True)
-        content_a, _ = encode_a2z(images)
+# Start testing
+for (real_A, name_A, real_B, name_B) in zip(data_loader_A, names_A, data_loader_B, names_B):
+    print([name_A[1], name_B[1]])
 
-        # decode latent to b
-        outputs_b = decode_z2b(content_a)
-        outputs_b = (outputs_b + 1) / 2.
+    # content_b_A means we started with image A, and the latent has been calculated from b->z
+    
+    # real_A -> Z -> fake_B
+    with torch.no_grad():
+        real_A =  real_A.cuda()
+        content_a_A, _ = encode_a2z(real_A)
+        fake_B = decode_z2b(content_a_A)
 
-        # re-encode b to latent space
-        content_b, _ = encode_b2z(outputs_b)
+        # fake_B -> Z -> rec_A
+        content_b_A, _ = encode_b2z(fake_B)
+        rec_A = decode_z2a(content_b_A)
 
-        # decode latent to a
-        recs_a = decode_z2a(content_b)
-        recs_a = (recs_a + 1) / 2.
-        
-        # path = os.path.join(opts.output_folder, 'input{:03d}_output{:03d}.jpg'.format(i, j))
-        basename = os.path.basename(names[1])
-        path = os.path.join(opts.output_folder,basename)
+        # real_B -> Z -> fake_A
+        real_B = real_B.cuda()
+        content_b_B, _ = encode_b2z(real_B)
+        fake_A = decode_z2a(content_b_B)
 
-        # make output folder
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
+        # fake_A -> Z -> rec_B
+        content_a_B, _ = encode_a2z(fake_A)
+        rec_B = decode_z2b(content_a_B)
 
-        # save output_b images
-        vutils.save_image(outputs_b.data, os.path.join(opts.output_folder, 'output_{}.jpg'.format(basename)), padding=0, normalize=True)
+    basename = os.path.basename(name_A[1])
 
-        # save input images
-        vutils.save_image(images.data, os.path.join(opts.output_folder, 'input_{}.jpg'.format(basename)), padding=0, normalize=True)
+    
+    # make dict of output images to be saved to site
+    visuals = OrderedDict()
+    visuals['real_A'] = real_A.data
+    visuals['fake_B'] = fake_B.data
+    visuals['rec_A'] = rec_A.data
+    visuals['real_B'] = real_B.data
+    visuals['fake_A'] = fake_A.data
+    visuals['rec_B'] = rec_B.data
+    
+    img_path = os.path.join(opts.output_folder,basename)
 
-        # also save rec images
-        vutils.save_image(recs_a.data, os.path.join(opts.output_folder, 'rec_{}.jpg'.format(basename)), padding=0, normalize=True)
-else:
-    pass
+    save_images(webpage, visuals, img_path, aspect_ratio=1, width=256)
+
+webpage.save()
+
+
+    # save output_b images
+    #vutils.save_image(outputs_b.data, os.path.join(opts.output_folder, 'output_{}.jpg'.format(basename)), padding=0, normalize=True)
+
+    # save input images
+    #vutils.save_image(images.data, os.path.join(opts.output_folder, 'input_{}.jpg'.format(basename)), padding=0, normalize=True)
+
+    # also save rec images
+    #vutils.save_image(recs_a.data, os.path.join(opts.output_folder, 'rec_{}.jpg'.format(basename)), padding=0, normalize=True)
